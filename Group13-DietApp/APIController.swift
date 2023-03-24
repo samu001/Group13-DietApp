55574842a8be7d5ca2391c3d2549aa7b86fffc96
Harry Hocker <harry_hocker@outlook.com>
2023-03-24 12:26:22 -0400

    Created generic API.

---
+++ Group13-DietApp/APIController.swift
@@ -0,0 +1,61 @@
+//
+//  APIController.swift
+//  Group13-DietApp
+//
+//  Created by Harry Hocker on 3/24/23.
+//
+
+import Foundation
+
+class recipeAPI: ObservableObject {
+    
+    @Published var results = [JSONRecipeResult.Recipe]()
+    var url: String!
+    
+    init(api: String) {
+        url = api
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: url) else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(JSONRecipeResult.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
+    }
+}
+
+
+class dietAPI: ObservableObject {
+    
+    @Published var results = [JSONDietResult.Diet]()
+    var url: String!
+    
+    init(api: String) {
+        url = api
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: url) else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(JSONDietResult.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
+    }
+}
