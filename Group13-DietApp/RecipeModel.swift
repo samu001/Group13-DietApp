cb8108f0dc07d97aa2545fe141f9bbbfe6a307d4
Harry Hocker <harry_hocker@outlook.com>
2023-03-24 21:32:31 -0400

    Added ingredients to both view and model. Added instructions to both view and model.

--- Group13-DietApp/RecipeModel.swift
+++ Group13-DietApp/RecipeModel.swift
@@ -15,6 +15,8 @@ struct JSONRecipeResult: Codable {
         let title: String
         let description: String
         let imageURL: String
+        let ingredients: [String]
+        let instructions: String
     }
 }
 
@@ -28,7 +30,11 @@ extension JSONRecipeResult.Recipe {
             id: 0,
             title: "Super Cookies",
             description: "These cookies will blow you away! You'll never want to give them up!",
-            imageURL: "https://www.enisa.europa.eu/news/enisa-news/2018bittersweet-cookies2019-new-types-of-2018cookies2019-raise-online-security-privacy-concerns/@@images/image"
+            imageURL:
+                "https://www.enisa.europa.eu/news/enisa-news/2018bittersweet-cookies2019-new-types-of-2018cookies2019-raise-online-security-privacy-concerns/@@images/image",
+            ingredients:
+                ["10 eggs", "3lbs bacon", "onion syrup", "flour"],
+            instructions: "Take the 10 eggs and put them in a bowl. Set the bowl outside. Then, eat the 3lbs of bacon raw. After 24 hours, use whatever is left and try to guess what you need to make this recipe work."
         )
     ]
 }
--- Group13-DietApp/SingleRecipeView.swift
+++ Group13-DietApp/SingleRecipeView.swift
@@ -26,16 +26,17 @@ struct SingleRecipeView: View {
                     },
                                placeholder: {
                         ProgressView(){
-                            Image("placeholder-image")
-                                .resizable()
-                                .scaledToFit()
-                                .background()
-                        } .progressViewStyle(.linear)
+                            Text("Loading Image...")
+                        } .progressViewStyle(.circular)
                     }
                     )
-                    HStack(alignment: .top) {
-                        Text(recipe.description).padding(.horizontal)
-                    }
+                }
+                VStack{
+                    Text(recipe.description).padding(.horizontal)
+                    Spacer()
+                    
+                    ingredientBox(recipe.ingredients)
+                    Text(recipe.instructions)
                 }
                 .navigationTitle(recipe.title).navigationBarTitleDisplayMode(.inline).padding()
             }
@@ -53,4 +54,24 @@ struct SingleRecipeView_Previews: PreviewProvider {
     static var previews: some View {
         SingleRecipeView(recipe: JSONRecipeResult.Recipe.exampleRecipes[0])
     }
+}
+
+
+func ingredientBox(_ ingredients: [String]) -> some View {
+    return
+        VStack {
+            Text("Ingredients")
+                .font(.title2)
+            VStack{
+                ForEach(ingredients, id: \.self) { string in
+                    Text(string)
+                }
+            }
+            .font(.title2)
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(lineWidth: 2)
+            )
+        }.padding()
 }
