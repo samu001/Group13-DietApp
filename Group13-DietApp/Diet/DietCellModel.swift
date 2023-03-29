b47b3381335f8a5e01240831dbcbba8ee2663ef2
Harry Hocker <harry_hocker@outlook.com>
2023-03-28 21:16:22 -0400

    Grouped files.

---
+++ Group13-DietApp/Diet/DietCellModel.swift
@@ -0,0 +1,20 @@
+//
+//  DietCellModel.swift
+//  Group13-DietApp
+//
+//  Created by Harry Hocker on 3/28/23.
+//
+
+import SwiftUI
+
+struct DietCellModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DietCellModel_Previews: PreviewProvider {
+    static var previews: some View {
+        DietCellModel()
+    }
+}
